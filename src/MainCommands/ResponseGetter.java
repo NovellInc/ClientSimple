ee55bec482d0438cc2ddcf019681d310350521b0
unknown <Omelchuk.Roman@UFA-W0026.infotecs-nt>
2016-01-26 17:51:37 +0500

    Создан поток приема сообщений

---
+++ src/MainCommands/ResponseGetter.java
@@ -0,0 +1,17 @@
+package MainCommands;
+
+import java.io.PrintWriter;
+
+/**
+ * Created by Omelchuk.Roman on 26.01.2016.
+ */
+public class ResponseGetter extends Thread {
+
+    private PrintWriter response;
+
+    @Override
+    public void run() {
+        super.run();
+
+    }
+}
